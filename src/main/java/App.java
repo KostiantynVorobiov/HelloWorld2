6b2fd3796b24abb778edbc144680ea1370608cd1
Kostian <pyteec.ua@i.ua>
2020-07-24 00:08:14 +0300

    Add method friends in new branch

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,4 +2,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!!!!");
     }
+    public static void friends(){
+        System.out.println("Hello mates!!!");
+    }
 }
